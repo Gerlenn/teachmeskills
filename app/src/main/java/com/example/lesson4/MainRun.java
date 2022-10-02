329addfb4918ebdff4b5a8e88674510302413c45
Gerlenn <gleb_kozlov1998@mail.ru>
2022-10-02 19:09:09 +0300

    Add class MainRun and Abstract class Bird

--- app/src/main/java/com/example/lesson4/MainRun.java
+++ app/src/main/java/com/example/lesson4/MainRun.java
@@ -14,12 +14,14 @@ class Pigeon extends Bird{
         System.out.println("The pigeon is flying");
     }
 }
+
 class Crow extends Bird{
     @Override
     void fly() {
         System.out.println("The crow is flying");
     }
 }
+
 abstract class Bird {
     abstract void fly();
 }
